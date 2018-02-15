23e31ceff2943bc0f7286d25dd14450a14b986af
dominic-wedesign <dominic@we-design.eu>
2018-02-15 12:16:51 +0100

    fix

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 import re
 
 version = ''
-with open('pyformsd/__init__.py', 'r') as fd: version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+with open('pyforms/__init__.py', 'r') as fd: version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
 																 fd.read(), re.MULTILINE).group(1)
 
 if not version: raise RuntimeError('Cannot find version information')
 
 setup(
-	name='PyFormsD',
+	name='PyForms',
 	version=version,
 	description="""Pyforms is a Python 2.7 and 3.4 framework to develop GUI application,
 		which promotes modular software design and code reusability with minimal effort.""",
@@ -23,18 +23,18 @@ setup(
 		"anyqt",
 	],
 	packages=[
-		'pyformsd',
-		'pyformsd.utils',
-		'pyformsd.terminal',
-		'pyformsd.terminal.Controls',
-		'pyformsd.gui',
-		'pyformsd.gui.dialogs',
-		'pyformsd.gui.Controls',
-		'pyformsd.gui.Controls.ControlEventTimeline',
-		'pyformsd.gui.Controls.ControlEventsGraph',
-		'pyformsd.gui.Controls.ControlPlayer'],
+		'pyforms',
+		'pyforms.utils',
+		'pyforms.terminal',
+		'pyforms.terminal.Controls',
+		'pyforms.gui',
+		'pyforms.gui.dialogs',
+		'pyforms.gui.Controls',
+		'pyforms.gui.Controls.ControlEventTimeline',
+		'pyforms.gui.Controls.ControlEventsGraph',
+		'pyforms.gui.Controls.ControlPlayer'],
 
-	package_data={'pyformsd': [
+	package_data={'pyforms': [
 		'gui/Controls/uipics/*.png',
 		'gui/mainWindow.ui', 'gui/Controls/*.ui', 'gui/Controls/ControlPlayer/*.ui',
 		'gui/Controls/ControlEventTimeline/*.ui']
